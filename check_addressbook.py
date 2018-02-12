025413814c4c729fb85aa6271d25fa522806dd1d
Kayser <kayser@4demkayser.ad.here.com>
2018-02-12 15:45:44 +0100

    asds

--- check_addressbook.py
+++ check_addressbook.py
@@ -1,9 +1,9 @@
 import sys
 from protobuf_out import addressbook_pb2
-# from protobuf-3.5.1 import python
 
 print "Hello."
 
+# initialization
 person = addressbook_pb2.Person()
 person.id = 1234
 person.name = "John Doe"
