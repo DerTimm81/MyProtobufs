0edefe6a53f988e761551f80488b508599b3e362
Kayser <kayser@4demkayser.ad.here.com>
2018-02-12 16:11:06 +0100

    added some output

--- check_addressbook.py
+++ check_addressbook.py
@@ -5,11 +5,21 @@ print "Hello."
 
 # initialization
 person = addressbook_pb2.Person()
+
 person.id = 1234
 person.name = "John Doe"
 person.email = "jdoe@example.com"
+
 phone = person.phones.add()
 phone.number = "555-4321"
 phone.type = addressbook_pb2.Person.HOME
 
-print person.id
+print "------------------------"
+print "OUTPUT:"
+print "Name of the person:\t " + person.name
+if phone.type is addressbook_pb2.Person.HOME :
+    print "Phone number is:\t " + phone.number + " (Home)"
+    pass
+
+# print "Given Phone number: " + person.phone.number + "(" + person.phone.type + ")"
+print "------------------------"
